eed02d35b57f0f551f6d68167a180563d0c16bfb
Akshar Bhatnagar <bhatnagarakshar@gmail.com>
2024-10-25 14:48:05 +0530

    till usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole]=useState();
+  const showEmployees=true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {showEmployees?(
+      <>
+      <input type='text' onChange={(e)=>{
+        console.log(e.target.value)
+        setRole(e.target.value)
+      }}/>
+          <Employee name="Akshar" role="developer"/>
+          <Employee name="Yash" role={role}/>
+          <Employee name="Arham" role="Designer"/>
+          </>
+        ):(
+          <h1>You cannot view</h1>
+          )}
+       
+      
     </div>
   );
 }
